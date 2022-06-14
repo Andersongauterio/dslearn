4b410263e313712f2681c905f71c6f3629dc7381
Andersongauterio <anderson.camara.gauterio@gmail.com>
2022-06-14 10:16:04 -0300

    Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!d','https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Logo_Course_Croisi%C3%A8re_EDHEC_noir.png/901px-Logo_Course_Croisi%C3%A8re_EDHEC_noir.png?20160102194649');
