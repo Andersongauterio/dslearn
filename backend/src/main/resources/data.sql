c33aec28d9e796faf3c74deffad7274ac3f8ede6
Andersongauterio <anderson.camara.gauterio@gmail.com>
2022-06-14 11:55:38 -0300

    Offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -16,3 +16,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!d','https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Logo_Course_Croisi%C3%A8re_EDHEC_noir.png/901px-Logo_Course_Croisi%C3%A8re_EDHEC_noir.png?20160102194649');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2022-06-14T03:00:00Z', TIMESTAMP WITH TIME ZONE '2023-06-14T03:00:00Z', 1); 
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2022-12-14T03:00:00Z', TIMESTAMP WITH TIME ZONE '2023-12-14T03:00:00Z', 1); 
